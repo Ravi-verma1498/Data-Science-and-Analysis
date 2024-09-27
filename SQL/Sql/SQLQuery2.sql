3649a828b71b5f99e05431fa588acddace4de763
Ravi-verma1498 <154362046+Ravi-verma1498@users.noreply.github.com>
2024-09-27 15:42:40 +0530

    Delete SQL/Sql/SQLQuery2.sql

--- SQL/Sql/SQLQuery2.sql
+++
@@ -1,7 +0,0 @@
-
-use PIZZA_Data;
-select * from pizza_sales;
-
-select datename(month,order_date)  as "Month_name"
-,count(distinct order_id) as "total orders" from
-group by datename(month, order_date)
